9e7e355d39d2ef69d935100f77d8e223f5b38fe6
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-03-03 18:43:56 -0500

    about hero svg fix

--- components/about/about-hero.tsx
+++ components/about/about-hero.tsx
@@ -1,7 +1,7 @@
 const AboutHero = () => {
 	return (
-		<section className='pt-[120px] relative'>
-			<div className='max-w-[1110px] mx-auto pb-[120px] grid grid-cols-[350px_730px] gap-[30px] relative'>
+		<section className='py-[120px] relative'>
+			<div className='max-w-[1110px] mx-auto grid grid-cols-[350px_730px] gap-[30px] relative'>
 				<div>
 					<h1 className='text-[64px] leading-[100px] max-w-[10ch]'>About</h1>
 				</div>
@@ -25,7 +25,7 @@ const AboutHero = () => {
 					<path fill='#2C6269' d='M100 100H0V0h100z' />
 					<path
 						fill='#F67E7E'
-						d='M100 0c55.228 0 100 44.77<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200"><g fill="none" fill-rule="evenodd"><path fill="#2C6269" d="M100 100H0V0h100z"/><path fill="#F67E7E" d="M100 0c55.228 0 100 44.772 100 100s-44.772 100-100 100S0 155.228 0 100 44.772 0 100 0zm0 67c-18.225 0-33 14.775-33 33s14.775 33 33 33 33-14.775 33-33-14.775-33-33-33z"/><path fill="#79C8C7" d="M100 40v4H0v-4h100zm0-8v4H0v-4h100zm0-8v4H0v-4h100zm0-8v4H0v-4h100zm0-8v4H0V8h100zm0-8v4H0V0h100z"/></g></svg>2 100 100s-44.772 100-100 100S0 155.228 0 100 44.772 0 100 0zm0 67c-18.225 0-33 14.775-33 33s14.775 33 33 33 33-14.775 33-33-14.775-33-33-33z'
+						d='M100 0c55.228 0 100 44.772 100 100s-44.772 100-100 100S0 155.228 0 100 44.772 0 100 0zm0 67c-18.225 0-33 14.775-33 33s14.775 33 33 33 33-14.775 33-33-14.775-33-33-33z'
 					/>
 					<path
 						fill='#79C8C7'
