49bd7e1539a05334f3329ed31c8a3e26c4f2dc17
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-03-04 16:53:00 -0500

    tablet header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -5,12 +5,12 @@ import Button from '../shared/button';
 
 const Header = () => {
 	return (
-		<header className='max-w-[1110px] mx-auto mt-[73px] flex justify-start items-center'>
+		<header className='md:max-w-[689px] lg:max-w-[1110px] mx-auto mt-[73px] flex justify-start items-center'>
 			<Image
 				src={logo}
 				alt='MyTeam Text Logo'
 				priority={true}
-				className='mr-20'
+				className='w-[128px] md:mr-12 lg:mr-20 lg:w-auto'
 			/>
 			<Nav />
 			<Button style='primary-light' link='/contact' label='contact us' />
