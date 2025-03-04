f790e88b67e000f38ace85729f36a579ba2bdcdf
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-03-04 08:43:21 -0500

    complete about page with prefooter

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
 import AboutHero from '@/components/about/about-hero';
 import AboutDirectors from '@/components/about/about-directors';
 import AboutClients from '@/components/about/about-clients';
+import PreFooter from '@/components/shared/pre-footer';
 const AboutPage = () => {
 	return (
 		<>
 			<AboutHero />
 			<AboutDirectors />
 			<AboutClients />
+			<PreFooter />
 		</>
 	);
 };
