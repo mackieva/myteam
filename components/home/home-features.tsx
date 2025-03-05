d55cd6e3dad909f7350d767c53266f54a0af6f85
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-03-05 13:14:00 -0500

    home features mobile responsiveness

--- components/home/home-features.tsx
+++ components/home/home-features.tsx
@@ -5,46 +5,50 @@ import Image from 'next/image';
 
 const HomeFeatures = () => {
 	return (
-		<section className='bg-ssg py-[100px] lg:py-[140px] relative'>
-			<div className='max-w-[573px] lg:max-w-[1110px] mx-auto flex flex-col justify-between items-start gap-16 lg:grid lg:grid-cols-[445px_540px] lg:gap-[125px]'>
-				<div className='flex flex-col justify-start items-start max-w-[445px] lg:max-w-auto'>
+		<section className='bg-ssg px-6 md:px-0 py-[64px] md:py-[100px] lg:py-[140px] relative'>
+			<div className='max-w-[573px] lg:max-w-[1110px] mx-auto flex flex-col justify-between items-start gap-14 md:gap-16 lg:grid lg:grid-cols-[445px_540px] lg:gap-[125px]'>
+				<div className='flex flex-col justify-start items-start max-w-[240px] md:max-w-[445px] lg:max-w-auto'>
 					<span className='h-1 w-[50px] bg-lc' />
 					<h2 className='text-[32px] leading-8 lg:text-[48px] lg:leading-12 mt-8 lg:mt-[54px]'>
 						Build & manage distributed teams like no one else.
 					</h2>
 				</div>
 				<div>
-					<div className='flex justify-between items-start gap-[23px] mb-8 max-w-[573px] lg:max-w-auto lg:mt-[54px]'>
+					<div className='flex flex-col md:flex-row justify-between items-center text-center gap-4 mb-12 md:items-start md:gap-[23px] md:text-left md:mb-8 max-w-[573px] lg:max-w-auto lg:mt-[54px]'>
 						<Image
 							src={person}
 							alt='Person Icon With Circle Background'
 							className='ml-[7px]'
 						/>
 						<div>
-							<p className='body-one text-lc mb-4'>Experienced Individuals</p>
+							<p className='body-one text-lc mb-2 md:mb-4'>
+								Experienced Individuals
+							</p>
 							<p className='body-two opacity-80'>
 								Our network is made up of highly experienced professionals who
 								are passionate about what they do.
 							</p>
 						</div>
 					</div>
-					<div className='flex justify-between items-start gap-[23px] mb-8'>
+					<div className='flex flex-col md:flex-row justify-between items-center text-center gap-4 mb-12 md:items-start md:gap-[23px] md:text-left md:mb-8 max-w-[573px] lg:max-w-auto lg:mt-[54px]'>
 						<Image src={cog} alt='Cog Icon With Circle Background' />
 						<div>
-							<p className='body-one text-lc mb-4'>Easy to Implement</p>
+							<p className='body-one text-lc mb-2 md:mb-4'>Easy to Implement</p>
 							<p className='body-two opacity-80'>
 								Our processes have been refined over years of implementation
 								meaning our teams always deliver.
 							</p>
 						</div>
 					</div>
-					<div className='flex justify-between items-start gap-[23px]'>
+					<div className='flex flex-col md:flex-row justify-between items-center text-center gap-4 md:items-start md:gap-[23px] md:text-left max-w-[573px] lg:max-w-auto lg:mt-[54px]'>
 						<Image
 							src={chart}
 							alt='Line and Bar Chart Icon With Circle Background'
 						/>
 						<div>
-							<p className='body-one text-lc mb-4'>Enhanced Productivity</p>
+							<p className='body-one text-lc mb-2 md:mb-4'>
+								Enhanced Productivity
+							</p>
 							<p className='body-two opacity-80'>
 								Our customized platform with in-built analytics helps you manage
 								your distributed teams.
