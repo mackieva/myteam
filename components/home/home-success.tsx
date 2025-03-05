95301170133d65a20bc9ba61dd81b67350cbbda4
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-03-05 08:04:51 -0500

    home success tablet responsiveness

--- components/home/home-success.tsx
+++ components/home/home-success.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const HomeSuccess = () => {
 	return (
-		<section className='py-[140px] relative'>
+		<section className='py-[100px] lg:py-[140px] relative'>
 			<svg
 				xmlns='http://www.w3.org/2000/svg'
 				width='147'
@@ -20,31 +20,33 @@ const HomeSuccess = () => {
 					/>
 				</g>
 			</svg>
-			<div className='max-w-[1110px] mx-auto'>
-				<h2 className='h2 text-center mb-14 text-balance'>
+			<div className='max-w-[573px] lg:max-w-[1110px] mx-auto'>
+				<h2 className='text-[32px] leading-[32px] lg:text-[48px] lg:leading-[48px] text-center mb-14 text-pretty lg:text-balance'>
 					Delivering real results for top companies. Some of our{' '}
-					<span className='text-rb'>success stories.</span>
+					<span className='text-rb text-nowrap lg:text-wrap'>
+						success stories.
+					</span>
 				</h2>
-				<div className='grid grid-cols-3 gap-[30px]'>
+				<div className='grid grid-cols-1 lg:grid-cols-3 gap-[30px]'>
 					<div className='flex flex-col justify-between items-center text-center pt-9 relative'>
 						<svg
 							xmlns='http://www.w3.org/2000/svg'
 							width='67'
 							height='56'
-							className='absolute top-0 left-[42%] -z-10'
+							className='absolute top-0 left-[50%] transform-[translateX(-50%)] -z-10'
 						>
 							<path
 								fill='#2C6269'
 								d='M51.273 56c4.638 0 8.368-1.322 11.19-3.967 2.823-2.645 4.235-5.799 4.235-9.46 0-2.95-.429-5.494-1.286-7.63a197.251 197.251 0 01-2.495-6.562c-.806-2.237-1.21-4.933-1.21-8.087 0-2.746.454-5.798 1.361-9.155C63.976 7.782 65.286 4.069 67 0H55.356c-3.73 3.662-7.032 7.833-9.905 12.512-2.873 4.68-5.166 9.359-6.88 14.038C36.857 31.23 36 35.4 36 39.063c0 5.391 1.361 9.562 4.083 12.512C42.805 54.525 46.535 56 51.273 56zm-36 0c4.638 0 8.368-1.322 11.19-3.967 2.823-2.645 4.235-5.799 4.235-9.46 0-2.95-.429-5.494-1.286-7.63a197.251 197.251 0 01-2.495-6.562c-.806-2.237-1.21-4.933-1.21-8.087 0-2.746.454-5.798 1.361-9.155C27.976 7.782 29.286 4.069 31 0H19.356c-3.73 3.662-7.032 7.833-9.905 12.512-2.873 4.68-5.166 9.359-6.88 14.038C.857 31.23 0 35.4 0 39.063c0 5.391 1.361 9.562 4.083 12.512C6.805 54.525 10.535 56 15.273 56z'
 							/>
 						</svg>
-						<p className='body-two mb-6'>
+						<p className='body-two mb-4 lg:mb-6'>
 							“The team perfectly fit the specialized skill set required. They
 							focused on the most essential features helping us launch the
 							platform eight months faster than planned.”
 						</p>
 						<p className='body-one text-rb'>Kady Baker</p>
-						<p className='text-[13px] italic mb-8'>
+						<p className='text-[13px] italic mb-4 lg:mb-8'>
 							Product Manager at Bookmark
 						</p>
 						<Image
@@ -58,7 +60,7 @@ const HomeSuccess = () => {
 							xmlns='http://www.w3.org/2000/svg'
 							width='67'
 							height='56'
-							className='absolute top-0 left-[42%] -z-10'
+							className='absolute top-0 left-[50%] transform-[translateX(-50%)] -z-10'
 						>
 							<path
 								fill='#2C6269'
@@ -83,7 +85,7 @@ const HomeSuccess = () => {
 							xmlns='http://www.w3.org/2000/svg'
 							width='67'
 							height='56'
-							className='absolute top-0 left-[42%] -z-10'
+							className='absolute top-0 left-[50%] transform-[translateX(-50%)] -z-10'
 						>
 							<path
 								fill='#2C6269'
